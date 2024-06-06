f5342ca71af01ad6a314a4ad595325c0a48da3a5
ivanprayogaadi <ivanprayogaadi@gmail.com>
2024-06-06 03:21:38 +0000

    add logo

--- src/components/Best.jsx
+++ src/components/Best.jsx
@@ -1,5 +1,6 @@
 import { Row, Col, Card, Container } from "react-bootstrap";
 import "../assets/styles/style.css";
+import * as Icon from "react-bootstrap-icons"
 import img1 from "../assets/images/hero.jpg"
 
 
@@ -11,9 +12,9 @@ const Best = () => {
       <Row className="justify-content-center" >
         <Col xs={12} sm={6} md={6} lg={4} xl={3} className="my-2 ">
           <Card className="custom-card">
-            <Card.Img variant="top" src={img1} />
+            <Icon.Award className="mx-auto mt-3 mb-4" width={100} height={100}/>
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>Award</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up the
                 bulk of the content.
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import { Button, Navbar, Nav, Container } from 'react-bootstrap';
 import '../assets/styles/style.css'
+import { BorderWidth } from 'react-bootstrap-icons';
 const NavigationBar = () => {
   return (
     <Navbar id='header' expand="md" variant='dark' fixed='top' >
       <Container>
-        <Navbar.Brand className='text-warning fw-bold fs-3' href="/">Sweet Parade</Navbar.Brand>
+        <Navbar.Brand className='text-warning fw-bold fs-3' href="/"><img src="/logo_brand.png" alt="Sweet Parade" width={50} height={50} /> Sweet Parade</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" >
 {/* Suggested code may be subject to a license. Learn more: ~LicenseLog:3841625084. */}
